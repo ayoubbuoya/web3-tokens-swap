caa159a67721373ca0bd04019fa6f7a2c35181c0
Ayoub Amer <ayouub.ameur@gmail.com>
2024-07-16 18:33:54 +0100

    feat: config hardhat networks

--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -1,8 +1,24 @@
-import type { HardhatUserConfig } from "hardhat/config";
+import { vars, type HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox-viem";
+
+// const INFURA_API_KEY = vars.get("INFURA_API_KEY");
+const POLYGONSCAN_API_KEY = vars.get("POLYGONSCAN_API_KEY");
+const ACCOUNT_PRIVATE_KEY = vars.get("ACCOUNT_PRIVATE_KEY");
 
 const config: HardhatUserConfig = {
   solidity: "0.8.24",
+  networks: {
+    hardhat: {
+      chainId: 1337,
+    },
+    polygon_amoy: {
+      url: "https://rpc-amoy.polygon.technology",
+      accounts: [ACCOUNT_PRIVATE_KEY],
+    },
+  },
+  etherscan: {
+    apiKey: POLYGONSCAN_API_KEY,
+  },
 };
 
 export default config;
